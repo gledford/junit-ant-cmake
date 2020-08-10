c07d90975b73c279038f83885f182f5637806bf3
Garry Ledford <garry.ledford@gmail.com>
2020-08-10 17:36:26 -0400

    First commit

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+public class ControllerTest {
+
+	@Test
+	public void testConstruction() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_PassingCondition() {
+		assertEquals(1,1);
+	}
+
+}
